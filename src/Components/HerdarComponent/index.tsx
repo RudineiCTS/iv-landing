ae728f4036578353c484c8a4aa0e64fe0a78dfaf
Rudinei Costa <costaaugusto177@outlook.com>
2025-03-10 23:18:57 -0300

    refactor

--- src/Components/HerdarComponent/index.tsx
+++ src/Components/HerdarComponent/index.tsx
@@ -1,21 +1,45 @@
 import { twMerge } from "tailwind-merge";
 
 interface HeaderComponentProps {
-  switchPage: (e:string) => void;
+  switchPage: (e: string) => void;
   className: string;
 }
-export function HeaderComponent({switchPage, className}:HeaderComponentProps ) {
-  function handleSwitchPage(e:string){
-    // console.log(e)
+export function HeaderComponent({
+  switchPage,
+  className,
+}: HeaderComponentProps) {
+  function handleSwitchPage(e: string) {
     switchPage(e);
   }
   return (
     <div
-      className={twMerge("flex max-w-xl mt-20 ml-10 gap-10 text-rosaEscuro", className)}
+      className={twMerge(
+        "flex max-w-xl mt-20 ml-10 gap-10 text-rosaEscuro",
+        className
+      )}
     >
-      <h1 className="cursor-pointer" id='home' onClick={(e)=>handleSwitchPage((e.target as HTMLAnchorElement).id)}>Home</h1>
-      <h1 className="cursor-pointer" id="consultancy"onClick={(e)=>handleSwitchPage((e.target as HTMLAnchorElement).id)} >Consultoria</h1>
-      <h1 className="cursor-pointer" id="about" onClick={(e)=>handleSwitchPage((e.target as HTMLAnchorElement).id)}> Sobre </h1>
+      <h1
+        className="cursor-pointer"
+        id="home"
+        onClick={(e) => handleSwitchPage((e.target as HTMLAnchorElement).id)}
+      >
+        Home
+      </h1>
+      <h1
+        className="cursor-pointer"
+        id="consultancy"
+        onClick={(e) => handleSwitchPage((e.target as HTMLAnchorElement).id)}
+      >
+        Consultoria
+      </h1>
+      <h1
+        className="cursor-pointer"
+        id="about"
+        onClick={(e) => handleSwitchPage((e.target as HTMLAnchorElement).id)}
+      >
+        {" "}
+        Sobre{" "}
+      </h1>
     </div>
   );
 }
